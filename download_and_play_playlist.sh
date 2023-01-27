69f3ff7466a97ec267b63065ee32e6457b729d8b
Jacek Kowalczyk <jacekkowalczyk82@gmail.com>
2023-01-27 07:56:50 +0100

    fixed download script

--- download_and_play_playlist.sh
+++ download_and_play_playlist.sh
@@ -4,13 +4,26 @@ PLAYLIST_URL=$1
 PLAYLIST_NAME=$2
 PLAYLIST_DIR=$3
 
+# Usage: 
+# ./download_and_play_playlist.sh 'https://www.youtube.com/playlist?list=PLpxLTu3yeWT9Vk2FRHMn1_LDY9q2YgVW1' moja-youtube-music-2 ~/private/music/youtube-dl-temp
+
+
 mkdir -p ${PLAYLIST_DIR}
 cd ${PLAYLIST_DIR}
 
+if [ -e ~/Music/${PLAYLIST_NAME}.txt ]; then 
+    echo "Music already downloaded to the given playlist file ~/Music/${PLAYLIST_NAME}.txt !!! "
+else
+
+for file_name in $(youtube-dl -f 251 --get-filename --restrict-filenames ${PLAYLIST_URL}); do 
+    echo $file_name
+    echo "${PLAYLIST_DIR}/${file_name} " >> ~/Music/${PLAYLIST_NAME}.txt
+
+done
+fi 
+
+echo "Download the playlist"
 youtube-dl -f 251 --restrict-filenames ${PLAYLIST_URL}
 
-for file in $(find . -name "*.webm") do 
-    echo "${PLAYLIST_DIR}/${file} " >> ~/Music/${PLAYLIST_NAME}.txt
- done
-
+echo "Random play the playlist "
 py-playtube.sh ~/Music/${PLAYLIST_NAME}.txt  --random 
